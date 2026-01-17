971f231b5992dad769ff7818d90c5e8703bd991f
Mendee95 <batsaihanenhmend0@gmail.com>
2026-01-17 23:46:57 +0800

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,13 @@
+
+export interface FormData {
+  name: string;
+  email: string;
+  platform: string;
+  audienceSize: string;
+  revenue: string;
+  currentMonetization: string[];
+  otherMonetization: string;
+  bottleneck: string;
+  triedMethods: string;
+  whyPartner: string;
+}
